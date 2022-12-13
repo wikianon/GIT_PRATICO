9264671bc138a74b5f64314cb183966dd15ceb23
Jean Zonta <jeanzonta777@hotmail.com.br>
2022-12-13 17:14:15 -0300

    script para subir repositorio local

--- git_pratico.sh
+++ git_pratico.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
-#
-# Script para criar subir projeto local
-# para repositório remoto github.com
 
+ACESS_TOKEN="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"
 
 GIT=$(which git)
 
+CURL=$(which curl)
+
 LINK="https://github.com"
 
-echo "Digite o nome do seu usuario do https://github.com/<seu_usuario_git>: "
-read USER_GIT
+echo "este script deve ser aberto dentro da pasta do projeto"
+echo
+read -p "Digite o nome do seu usuario do github: " USER_GIT
+echo
 
-USER_LINK=$LINK/$USER_GIT
+read -p "Digite o nome da pasta original do seu projeto: " PROJECT_NAME
 
-read -p "Digite o caminho da pasta do projeto: " CAMINHO
 
-cd $CAMINHO
+#Este comando cria um repositorio remoto sem precisar entrar no github
 
+$CURL -i -H "Authorization: token $ACESS_TOKEN" -d "{\"name\":\"$PROJECT_NAME\",\"private\":true}" https://api.github.com/user/repos
+
+GIT_LINK=$LINK/$USER_GIT
 
 $GIT init
 
 $GIT add .
 
-echo "Digite um comentario da sua ação"
-read COMENTARIO
+echo "Digite um comentario sobre o que voçẽ fez no projeto"
+echo
+read -p "Entre com o comentario: " COMENTARIO
 
+##Comitando o projeto
+#
+$GIT commit -m "$COMENTARIO"
+
+
+## Linkando nosso projeto local com o projeto remoto
+#
+echo "git remote add origin $GIT_LINK/$PROJECT_NAME.git"
+$GIT remote add origin $GIT_LINK/$PROJECT_NAME.git
 
 $GIT commit -m "$COMENTARIO"
 
-echo "Digite exatamente o mesmo nome do projeto"
-echo "desde que voçê ja tenha criado este projeto"
-echo "no https://github.gom/<usuario>/<meuprojeto>"
-
-read NOME_PROJETO
-
-$GIT remote add origin $USER_LINK/$NOME_PROJETO.git
 
 echo "Agora digite git push origin master e entre com seu usuario e o Token"
 exit
